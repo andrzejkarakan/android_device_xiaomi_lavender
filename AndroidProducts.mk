AmulyaX/device_xiaomi_lavender

lavender: AOSiP Bringup
Signed-off-by: AmulyaX <amulya.b520@gmail.com>

AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_lavender.mk
+    $(LOCAL_DIR)/aosip_lavender.mk

andrzejkarakan/android_device_xiaomi_lavender

AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_lavender.mk
+    $(LOCAL_DIR)/aosip.mk